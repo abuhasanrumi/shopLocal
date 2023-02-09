afb35bc0703d1f30ac0724802bbe5aa4e2fa2f84
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-02-09 18:50:50 +0600

    header strip done

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <div className="header-top-strip py-3">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-6">
+                            <p className='text-white mb-0'>Free Shipping Over $100 & Free Returns</p>
+                        </div>
+                        <div className="col-6">
+                            <p className='text-end text-white mb-0'>Hotline: <a className='text-white' href="tel:+8801537485726">+880 14234 24234</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
