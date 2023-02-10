74b22f5f80cae6e487e99b29388bf263c2dd1986
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-02-10 21:49:17 +0600

    made tweaks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
                                 <img src="images/main-banner-1.jpg" className="img-fluid rounded-3 w-100" alt="main banner" />
                                 <div className="main-banner-content position-absolute">
                                     <span className='main-banner-span fs-xsm'>SUPERCHARGED FOR PROS.</span>
-                                    <h4 className='main-banner-title fs-lg py-md-3'>iPad S13+ Pro.</h4>
+                                    <h4 className='main-banner-title fs-xl py-md-3'>iPad S13+ Pro.</h4>
                                     <p className='main-banner-details fs-sm'>From $999.00 or $41.62/mo.<br></br>for 24 mo. Footnote*</p>
                                     <Link><button type="button" class="btn darkButton py-1 py-sm-2 px-3 px-sm-4 rounded-pill">Buy Now</button></Link>
                                 </div>
