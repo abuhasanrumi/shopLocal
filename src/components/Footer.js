5de39f5b03ddce9a8ebf0e983bfc7d68ae3d2cae
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-02-10 21:37:32 +0600

    made footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,13 @@ const Footer = () => {
             <footer className="py-5">
                 <div className="container-xxl">
                     <div className="row align-items-center">
-                        <div className="col-5">
-                            <div className="footer-top-data d-flex gap-15 align-items-center">
+                        <div className="col-12 col-lg-5">
+                            <div className="footer-top-data d-flex gap-15 align-items-center justify-content-center pb-4 pb-lg-0">
                                 <img src="images/newsletter.png" alt="newsletter" />
                                 <h3 className='mb-0 text-white fw-light'>Sign Up For Newsletter</h3>
                             </div>
                         </div>
-                        <div className="col-7">
+                        <div className="col-10 col-lg-7 mx-auto">
                             <div className="input-group">
                                 <input type="text" className="form-control py-1 footer-sub" placeholder="Your email address" aria-label="Your email address" aria-describedby="basic-addon2" />
                                 <span className="input-group-text p-2" id="basic-addon2">
@@ -25,12 +25,12 @@ const Footer = () => {
                     </div>
                 </div>
             </footer>
-            <footer className="py-5">
+            <footer className="py-5 d-none d-lg-block">
                 <div className="container-xxl">
                     <div className="row">
                         <div className="col-4">
                             <h4 className='text-white mb-4'>Contact Us</h4>
-                            <div className='d-flex flex-column'>
+                            <div className='d-flex flex-column fs-6'>
                                 <address className='text-white fs-6'>No. 1259 Freedom, New York, 11111 United States</address>
                                 <Link className="text-white my-2" to="tel:+8801938472638">+8801938472638</Link>
                                 <Link className="text-white my-2" to="mailto:contact@shoplocal.com">contact@shoplocal.com</Link>
@@ -53,7 +53,7 @@ const Footer = () => {
                         <div className="col-3">
                             <h4 className='text-white mb-4'>Information</h4>
                             <div>
-                                <div className='footer-links d-flex flex-column'>
+                                <div className='footer-links d-flex flex-column fs-6'>
                                     <Link className='text-white py-2 mb-1' to="">Privacy Policy</Link>
                                     <Link className='text-white py-2 mb-1' to="">Refund Policy</Link>
                                     <Link className='text-white py-2 mb-1' to="">Shipping Policy</Link>
@@ -64,7 +64,7 @@ const Footer = () => {
                         <div className="col-3">
                             <h4 className='text-white mb-4'>Account</h4>
                             <div>
-                                <div className='footer-links d-flex flex-column'>
+                                <div className='footer-links d-flex flex-column fs-6'>
                                     <Link className='text-white py-2 mb-1' to="">About Us</Link>
                                     <Link className='text-white py-2 mb-1' to="">FAW</Link>
                                     <Link className='text-white py-2 mb-1' to="">Contact</Link>
@@ -74,7 +74,7 @@ const Footer = () => {
                         </div>
                         <div className="col-2">
                             <h4 className='text-white mb-4'>Quick Links</h4>
-                            <div className='footer-links d-flex flex-column'>
+                            <div className='footer-links d-flex flex-column fs-6'>
                                 <Link className='text-white py-2 mb-1' to="">Laptops</Link>
                                 <Link className='text-white py-2 mb-1' to="">Headphones</Link>
                                 <Link className='text-white py-2 mb-1' to="">Tablets</Link>
@@ -88,7 +88,7 @@ const Footer = () => {
                 <div className="container-xxl">
                     <div className="row">
                         <div className="col-12">
-                            <p className="text-center mb-0 text-white">&copy; {new Date().getFullYear()}; Powered by Abu Hasan Rumi</p>
+                            <p className="text-center mb-0 text-white fs-6">&copy; {new Date().getFullYear()}; Powered by Abu Hasan Rumi</p>
                         </div>
                     </div>
                 </div>
