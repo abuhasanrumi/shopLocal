5655315eb26923a5c12edd048dce9faf3293a98b
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-02-16 10:29:01 +0600

    added store

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -8,6 +8,14 @@ const OurStore = () => {
         <>
             <Meta title={"Our Store"} />
             <BreadCrumb title={"Our Store"} />
+            <div className="store-wrapper home-wrapper-2 py-5">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-3"></div>
+                        <div className="col-9"></div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
