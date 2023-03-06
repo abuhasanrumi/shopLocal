67aacdac9cbe4572612686a9df9796bbbc120eb8
Abu Hasan Rumi <abu.hasan.rumi@gmail.com>
2023-03-07 01:03:43 +0600

    comparison page bootstrap setup

--- src/pages/CompareProducts.js
+++ src/pages/CompareProducts.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import BreadCrumb from '../components/BreadCrumb'
+import Meta from '../components/Meta'
 
 const CompareProducts = () => {
     return (
-        <div>CompareProducts</div>
+        <>
+            <Meta title={"Compare Products"} />
+            <BreadCrumb title={"Compare Products"} />
+            <div className="compare-product-wrapper gray-bg p-4">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-6 col-lg-3 bg-white">
+                            <h2>Test</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
